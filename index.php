875f29ec473e6a0a72661cd9132b1684b6948af3
Valentin Brebion <vbrebion@dombtsig.local>
2022-09-14 14:49:53 +0200

    Ajout index.php

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
        <meta charset="utf-8">
         <!-- importer le fichier de style -->
         <link rel="stylesheet" href="style.css" media="screen" type="text/css" />
+        <!-- CSS only -->
+
     </head>
     <body>
         <div id="container">
